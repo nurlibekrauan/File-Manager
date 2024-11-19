a9d29d5f1a3c20884a59373620f3850b663b9e16
nurlibekrauan <nurlibekrauan2010@gmail.com>
2024-11-19 19:48:13 +0500

    Add files via upload

---
+++ __pyA.py
@@ -0,0 +1,32 @@
+from _pyA.utils import FileManager
+
+def menu():
+    print("Creating files...\n1. Create file\n2. Create files\n3. Create directory\n4. Create directories\n5. Delete by path")
+    value = int(input("Enter the choise"))
+    match value:
+        case 1:
+            file_manager = FileManager()
+            file_name = input("Enter the name of the file: ")
+            file_manager.create_file(file_name)
+        case 2:
+            file_manager = FileManager()
+            file_names = input("Enter the names of the files (separated by comma): ").split(',')
+            file_manager.create_files(file_names)
+        case 3:
+            directory_name = input("Enter the name of the directory: ")
+            file_manager = FileManager()
+            file_manager.create_directory(directory_name)
+        case 4:
+            directory_names = input("Enter the names of the directories (separated by comma): ").split(',')
+            file_manager = FileManager()
+            file_manager.create_directories(directory_names)
+        case 5:
+            file_path = input("Enter the path to the file/directory: ")
+            file_manager = FileManager()
+            file_manager.delete_by_path(file_path)
+        case _:
+            print("Invalid choice. Please try again.")
+if __name__ == '__main__':
+    while True: 
+        menu()
+
