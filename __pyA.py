9d9b10c45df2a7fbbd611f6f091d2e75074d40ba
nurlibekrauan <nurlibekrauan2010@gmail.com>
2024-11-21 21:03:22 +0500

    creating the main function

--- __pyA.py
+++ __pyA.py
@@ -1,24 +1,31 @@
 from _pyA.utils import FileManager
 
+
 def menu():
-    print("Creating files...\n1. Create file\n2. Create files\n3. Create directory\n4. Create directories\n5. Delete by path\b6. Delete directory")
+    print(
+        "Creating files...\n1. Create file\n2. Create files\n3. Create directory\n4. Create directories\n5. Delete by path\b6. Delete directory"
+    )
     value = int(input("Enter the choise"))
     match value:
         case 1:
             file_manager = FileManager()
             file_name = input("Enter the name of the file: ")
             file_extention = input("Enter the exctension to the file: ")
-            file_manager.create_file(file_name,file_extension=file_extention)
+            file_manager.create_file(file_name, file_extension=file_extention)
         case 2:
             file_manager = FileManager()
-            file_names = input("Enter the names of the files (separated by comma): ").split(',')
+            file_names = input(
+                "Enter the names of the files (separated by comma): "
+            ).split(",")
             file_manager.create_files(file_names)
         case 3:
             directory_name = input("Enter the name of the directory: ")
             file_manager = FileManager()
             file_manager.create_directory(directory_name)
         case 4:
-            directory_names = input("Enter the names of the directories (separated by comma): ").split(',')
+            directory_names = input(
+                "Enter the names of the directories (separated by comma): "
+            ).split(",")
             file_manager = FileManager()
             file_manager.create_directories(directory_names)
         case 5:
@@ -31,6 +38,12 @@ def menu():
             file_manager.delete_directory(directory_path)
         case _:
             print("Invalid choice. Please try again.")
-if __name__ == '__main__':
-    while True: 
-        menu()    
+
+
+def main():
+    menu()
+
+
+if __name__ == "__main__":
+    while True:
+        main()
